35030be90869bb652f90ec3c2f471b5fae919c38
zia <letscode786@gmail.com>
2021-11-28 14:22:00 +0500

    initial commit Equation used is 60x^3*(1-x)^2

---
+++ AcceptanceAndRejection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceAndRejection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input number of variates");
+                float[] Variates = new float[Convert.ToInt32(Console.ReadLine())];
+                float tX = 2.0736f;
+                int counter = 0;
+                for (int i = 0; ; i++)
+                {
+                    float randomNumber = (float)new Random().NextDouble();
+                    float temp = (float)(60 * Math.Pow(randomNumber, 3) * Math.Pow((1 - randomNumber), 2));
+                    if(randomNumber <= temp & !Variates.Contains(temp))
+                    {
+                        Variates[counter] = temp;
+                        counter++;
+                    }
+                    if (counter == Variates.Length)
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
